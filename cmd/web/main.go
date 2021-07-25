26edb3e18628e18ef384f64246e756e764780e2e
Verma, Ashwani <Ashwani.Verma@adidas.com>
2021-07-25 21:29:42 +0530

    29. Building a template cache (part two)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,7 @@ const portNumber = ":8080"
 
 func main() {
 	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
