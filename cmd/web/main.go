6ed80a38b1ded89c8f66b90eb30d1dfdc6fc28c1
Verma, Ashwani <Ashwani.Verma@adidas.com>
2021-07-30 15:08:52 +0530

    30. Setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/Ashwani00002/go-course/pkg/config"
 	"github.com/Ashwani00002/go-course/pkg/handlers"
+	"github.com/Ashwani00002/go-course/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.Appconfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
